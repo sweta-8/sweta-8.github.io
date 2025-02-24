4788b0e997c6b67ca2bcfd71d8ac240df11760fe
SWETA <86958897+sweta-8@users.noreply.github.com>
2025-02-24 07:46:54 +0000

    new button

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function showContent(sectionId) {
     var selectedSection = document.getElementById(sectionId);
     selectedSection.style.display = 'block';
 }
+
+function nextTab(currentSectionId) {
+    var menuItems = document.querySelectorAll('#sidebar-menu li a');
+    for (var i = 0; i < menuItems.length; i++) {
+        if (menuItems[i].getAttribute('href').substring(1) === currentSectionId) {
+            var nextIndex = (i + 1) % menuItems.length;
+            var nextSectionId = menuItems[nextIndex].getAttribute('href').substring(1);
+            showContent(nextSectionId);
+            break;
+        }
+    }
+}
